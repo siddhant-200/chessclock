3e8f71bd0bafad4cc080c55600219d99e3d12597
Siddhant Jindal <siddhantjindal20@gmail.com>
2020-10-01 15:12:39 +0530

    positioning the elements and stared working on the bas logic

--- chessclock.js
+++ chessclock.js
@@ -1,1 +1,15 @@
-alert("chessclock.js connected!!");
+
+var time1 = document.getElementById("time1");
+var time2 = document.getElementById("time2");
+
+
+time1.textContent = "10";
+time2.textContent = "10";
+
+
+
+
+
+
+
+
